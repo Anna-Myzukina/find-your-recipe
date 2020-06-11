139ad77163cec5c95b11053d7313243017d61c5d
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-06-11 18:55:21 +0300

    Create user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_132152) do
+ActiveRecord::Schema.define(version: 2020_06_11_155350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2020_06_09_132152) do
     t.text "description", null: false
     t.string "image", default: "https://github.com/Anna-Myzukina/microgreen/blob/master/src/img/easy-microgreens.jpg"
     t.integer "calories", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
